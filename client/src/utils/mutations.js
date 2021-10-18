779a20405550c3a2f22387657a9aac4427c24e58
Mayuri Pendyala <mayu04@gmail.com>
2021-10-18 18:07:36 +1100

    added the whole project

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER =gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        profile {
+            _id
+            name
+        }
+    }
+}
+`;
+
+export const ADD_USER =gql`
+    mutation addUser($username: String!,$password: String!,$email: String!) {
+        addUser(username: $username,password: $password,email: $email) {
+            token
+            user {
+                username
+                _id
+                email
+            }
+
+        }
+
+    }
+`;
+
+export const SAVE_BOOKS = gql`
+    mutation saveBook($input: SaveBookInput){
+        saveBook(input: $input) {
+            username
+            _id
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,82 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent, { userID }) => {
+      return User.findOne({ _id: userID });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user with this email found!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { input}, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet : { saveBooks: input}},
+            { new: true, runValidators: true }
+        );
+            return updatedUser;
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Set up mutation so a logged in user can only remove the book
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return Book.findOneAndDelete({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Make it so a logged in user can only remove a skill from their own profile
+    removeSkill: async (parent, { skill }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId} } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
